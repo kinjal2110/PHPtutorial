2fabeba6cbda9466fbd572deb907daead1e71d9d
kinjal2110 <64691460+kinjal2110@users.noreply.github.com>
2020-10-03 10:30:01 +0530

    Update asso_array.php

--- asso_array.php
+++ asso_array.php
@@ -2,7 +2,7 @@
 	//associative array
 	$a=array("Sachin"=>1000, "yuvraj"=>300,"virat"=>500);
 	$b=array(10=>'A',20=>'B',30=>'C');
-	//echo $a["Sachin"]; //if we print single value
+	//echo $a["Sachin"];     //if we print single value
 	print_r($a);
 	print_r($b);
 ?>
